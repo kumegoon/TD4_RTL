69c136f5e81938617a8695d7723c2b773c9cbc6c
SHINSUKE KUME <shinsuke.kume.j8z@jp.denso.com>
2019-08-23 13:30:04 +0900

    register test bench make now

--- RESISTER/register_tb.v
+++ RESISTER/register_tb.v
@@ -22,6 +22,55 @@ module register_tb;
 // Design set
 
     register register(
-        
+        .CLK(CLK),
+        .RST(RST),
+        .LOAD(LOAD),
+        .IN_DATA(IN_DATA),
+        .OUT_A(OUT_A),
+        .OUT_B(OUT_B),
+        .OUT_C(OUT_C),
+        .ADDRESS(ADDRESS)
+        );        
+
+//cycle set
+    event   cycle_event;
+    integer cycle_count;
+
+    initial
+        begin
+            -> cycle_event;
+            #1;
+            cycle_count = 0 ;
+        end
+
+    always
+        begin
+            #(`cycle);
+            -> cycle_event;
+        end
+
+    always @( cycle_event )
+        begin
+            cycle_count <= cycle_count + 1;
+                if (cycle_count % 1000 == 0) begin
+                    $display ( "%d cycle" , cycle_count);
+                end
+        end
+
+// test
+
+    initial
+        begin
+            #(`cycle);
+            IN_DATA <= "1010";
+            #(`cycle);
+            LOAD <= "1110";
+            #(`cycle);
+            LOAD <= "1101";
+            #(`cycle);
+
+            RST <= '1';
+            
+
 
     )
